89d2e5c579638b986aff546357b172c4342d7f33
= <=>
2019-09-16 15:06:33 -0700

    Sort and filter full function

--- components/LanguagesForm.jsx
+++ components/LanguagesForm.jsx
@@ -4,32 +4,18 @@ export default class LanguagesForm extends React.Component {
     constructor(props) {
         super(props);
         this.props = props;
-        this.onSubmit = this.onSubmit.bind(this);
-        this.onChange = this.onChange.bind(this);
-        this.onReset = this.onReset.bind(this);
-    }
-
-    onSubmit(e) {
-        e.preventDefault();
-        this.props.onSave && this.props.onSave.call(this);
-    }
-
-    onChange(e) {
-        e.preventDefault();
-        this.props.onChange && this.props.onChange(e.target);
-    }
-
-    onReset(e) {
-        e.preventDefault();
-        this.props.onReset && this.props.onReset(e.target);
     }
 
     render() {        
         return (            
-            <form onSubmit={this.onSubmit} onReset={this.onReset}>
-                <input id="addButt" type="text" name="language" placeholder="language" value={this.props.language} onChange={this.onChange} />
-                <input type="submit" value="Submit" />
-                <input type="reset" value="Cancel" />
+            <form onSubmit={this.props.onSave} onReset={this.props.onReset}>
+                <input id="addButt" type="text" name="language" placeholder="language" value={this.props.language} onChange={this.props.onChange} />
+                <input type="submit" value="Add" />
+                <input type="reset" value="Clear" />
+                <br />
+                <br />
+                <br />
+                <br />
             </form>
         )
     }
--- components/LanguagesPage.jsx
+++ components/LanguagesPage.jsx
@@ -1,23 +1,30 @@
 import * as React from "react";
 import * as axios from "axios";
 import LanguagesList from "./LanguagesList"
-import LanguagesForm from "./LanguagesForm"
+import LanguagesForm from "./LanguagesForm";
 
 const getNewLang = () => ({language: "", count: 0});
 
 export default class LanguagesPage extends React.Component {
     constructor(props) {
         super(props);
-        this.state = { adding: false, languages: [], newLanguage: getNewLang() };
+        this.state = {
+            adding: false,
+            languages: [],
+            newLanguage: getNewLang(),
+            showSearchButton: true
+        };
         this.props = props;
         this.onChange = this.onChange.bind(this);
+        this.filter = this.filter.bind(this);
+        this.getView = this.getView.bind(this);
         this.onIncrement = this.onIncrement.bind(this);
         this.onDecrament = this.onDecrament.bind(this);
-        this.onCancel = this.onCancel.bind(this);
+        this.onReset = this.onReset.bind(this);
         this.onRemove = this.onRemove.bind(this);
         this.onSave = this.onSave.bind(this);
         this.onShowAdd = this.onShowAdd.bind(this);
-        this.sortItems = this.sortIems.bind(this);
+        this.compare = this.compare.bind(this);
     }
 
     clearBox(id) {
@@ -34,10 +41,13 @@ export default class LanguagesPage extends React.Component {
             .then(() => this.load());
     }
 
-    onChange(target) {
-        let newLanguage = {...this.state.newLanguage};
-        newLanguage[target.name] = target.value;
-        this.setState( {newLanguage: newLanguage} );
+    onChange(e) {
+        let newLang = {...this.state.newLanguage};
+        newLang.language = e.target.value;
+        this.setState({
+            newLanguage: newLang,
+            sortLangs: this.getView(this.state.languages)
+        });
     }
 
     onRemove(language) {
@@ -49,60 +59,85 @@ export default class LanguagesPage extends React.Component {
         axios.post("/api/languages/", this.state.newLanguage)
         .then ( () => this.load() )
         .then(this.setState({ newLanguage: getNewLang(), adding: false}))
-        .then(document.getElementById("addButt").value = "")
-        .catch(
+        .then(this.clearBox("addButt"))
+        .then(this.onShowAdd())
+        .catch(() => {
+            if(err) {
+                alert(err);
+            }
+        }
             //todo: set an error condition
         )
     }
 
     onShowAdd() {
-        this.setState({ adding: !this.state.adding ? true : false });
+        this.setState({ adding: !this.state.adding ? true : false, showSearchButton: !this.state.showSearchButton ? true : false });
     }
 
-    onCancel() {
-        this.setState({ newLanguage: getNewLang(), adding: false});
+    onReset(e) {
+        e.preventDefault();
+        this.props.onReset && this.props.onReset(e.target);
+        this.onShowAdd();
     }
     
     componentDidMount() {
         this.load();
     }
 
-    sortIems(target) {
-        return target.sort((a, b) => {
+    filter(languages) {
+        return languages.language.toUpperCase().startsWith(this.state.newLanguage.language.toUpperCase());
+    }
+
+    compare(a, b) {
         if (a.language.toUpperCase() < b.language.toUpperCase()) { return -1; };
         if (a.language.toUpperCase() > b.language.toUpperCase()) { return 1; };
         return 0;
-        })
+    }
+
+    getView (languages) {
+        return languages.filter(this.filter).sort(this.compare);
     }
 
     async load() {
         let response = await axios.get("/api/languages");
-        this.setState({ languages: this.sortItems(response.data) });
+        this.setState({
+            sortLangs: this.getView(response.data),
+            languages: response.data
+        })
     }
 
     render() {
+        const view = this.getView(this.state.languages);
         return (
             <div>
                 {this.state.adding && 
                     <LanguagesForm 
-                        languages={this.state.newLanguage.language} 
-                        onChange={this.onChange} 
-                        onSave={this.onSave} 
-                        onReset={this.onCancel} /> 
+                        onChange={this.onChange}
+                        onSave={this.onSave}
+                        onReset={this.onReset} 
+                    />
                 }
-                <button onClick={() => this.onShowAdd()}>
-                    Add Language
-                </button>
-                {this.state.languages && 
-                this.state.languages.length && 
+                {this.state.showSearchButton && 
+                    <div>
+                        <button onClick={() => this.onShowAdd()}>
+                            Search...
+                        </button>
+                        <br />
+                        <br />
+                        <br />
+                        <br />
+                    </div>
+                }
+                {view && 
+                view.length && 
                     <LanguagesList 
-                        languages={this.state.languages} 
+                        languages={view} 
                         onIncrement={this.onIncrement} 
                         onDecrament={this.onDecrament}
                         onRemove={this.onRemove}
                     /> 
                 }
-                {this.state.languages.length <= 0 &&
+                {view.length <= 0 &&
                     <h1>There are no items to display!!</h1>
                 }
             </div>
